e23b60fa1f9df97a595413be3651d2aa4b547ecb
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-14 17:37:20 -0600

    Server started

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-$null 
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.get('/api/todos', function (req, res) {
+    res.json({todos:[]});
+});
+
+app.listen(3000, function () {
+    console.log('The server is running on port 3000');
+});
