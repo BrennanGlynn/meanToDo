543440da490ea31fb2fc7f24732e9b9ce136d49a
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-14 17:54:26 -0600

    Moved router to seperate file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 app.use('/', express.static('public'));
 
-app.get('/api/todos', function (req, res) {
-    res.json({todos:[]});
-});
+app.use('/api', router);
 
 app.listen(3000, function () {
-    console.log('The server is running on port 3000');
+    console.log('The server is running at http://localhost:3000/');
 });
