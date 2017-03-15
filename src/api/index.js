8f9b833eaf5269df850df9a5fa4734546bae9aed
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-15 14:20:58 -0600

    Implemented todos to use our API

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var express = require('express');
-
+var todos = require('../../mock/todos.json');
 var router = express.Router();
 
 router.get('/todos', function (req, res) {
-    res.json({todos:[]});
+    res.json({todos: todos});
+});
+
+router.get('/songs', function (req, res) {
+    res.json({songs: []})
 });
 
 // TODO: Add POST route to create new entries
